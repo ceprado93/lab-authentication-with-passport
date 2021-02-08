b2ce04da5ff7004422db2891496d46694c5b3790
Teo <teodorolg130@gmail.com>
2021-02-08 07:49:55 +0100

    updated boilerplate

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 // Require user model
 
 // Add bcrypt to encrypt passwords
 
 // Add passport
 
-const ensureLogin = require('connect-ensure-login');
+const ensureLogin = require('connect-ensure-login')
 
-router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
-});
+router.get('/private-page', (req, res) => {
+	res.render('auth/private', { user: req.user })
+})
 
-module.exports = router;
+module.exports = router
